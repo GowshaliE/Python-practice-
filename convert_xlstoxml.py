b8a784bfd143dd442be0baf585bcbe726cb97ed9
E GOWSHALI <115982885+GowshaliE@users.noreply.github.com>
2023-01-05 11:19:49 +0530

    Add files via upload

--- convert_xlstoxml.py
+++ convert_xlstoxml.py
@@ -1,5 +1,5 @@
 
-
+## method 1
 import xlrd
 
 # Open the Workbook
@@ -8,5 +8,21 @@ workbook = xlrd.open_workbook("/home/gowshali/Documents/files/IE000QOU09J7.xls")
 # Open the worksheet
 worksheet = workbook.sheet_by_index(0)
 
-
+## method 2
     
+from openpyxl import load_workbook
+from yattag import Doc, indent
+  
+
+wb = load_workbook("/home/gowshali/Documents/files/IE000QOU09J7.xls")
+ws = wb.worksheets[0]
+  
+
+doc, tag, text = Doc().tagtext()
+  
+xml_header = '<?xml version="1.0" encoding="UTF-8"?>'
+
+  
+
+doc.asis(xml_header)
+
