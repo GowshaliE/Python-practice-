e75e1c345ea37a13d48cb41b349d5a295b1a5093
E GOWSHALI <115982885+GowshaliE@users.noreply.github.com>
2023-01-05 11:12:10 +0530

    Add files via upload

---
+++ convert_xlstoxml.py
@@ -0,0 +1,12 @@
+
+
+import xlrd
+
+# Open the Workbook
+workbook = xlrd.open_workbook("/home/gowshali/Documents/files/IE000QOU09J7.xls")
+
+# Open the worksheet
+worksheet = workbook.sheet_by_index(0)
+
+
+    
